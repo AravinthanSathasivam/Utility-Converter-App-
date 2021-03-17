0549934adb800346b5fec74636c0c70ac7fbf0a7
Aravinthan Sathasivam <aravinthan0015@gmail.com>
2021-03-17 12:36:30 -0700

    new changes

---
+++ CW01/Controllers/SettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingViewController.swift
+//  CW01
+//
+//  Created by user188409 on 3/17/21.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    var decimalValue = 0 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+	
--- CW01/Controllers/WeightViewController.swift
+++ CW01/Controllers/WeightViewController.swift
@@ -21,6 +21,7 @@ class WeightViewController: UIViewController , UITextFieldDelegate{
     @IBOutlet weak var sPoundTextField: UITextField!
     @IBOutlet weak var kgTextField: UITextField!
     
+    
     //creating an object - Weight
     
     var weight : Weight = Weight(kg: 0.0, gram: 0.0, ounce: 0.0, pound: 0.0, stone: 0.0, s_pound: 0.0)
@@ -47,7 +48,7 @@ class WeightViewController: UIViewController , UITextFieldDelegate{
         textField.inputView = UIView()
     }
     
-    @IBAction func handleTextEdit(_ sender: UITextField) {
+    @IBAction func handleTextField(_ sender: UITextField) {
         guard let textFieldValue = sender.text else { return }
         guard let doubleTextFieldValue = Double(textFieldValue) else {
             ounceTextField.text = ""
@@ -60,44 +61,13 @@ class WeightViewController: UIViewController , UITextFieldDelegate{
         
         switch WeightScales(rawValue: sender.tag)! {
         
-        case .kg:
-            weight.kg = doubleTextFieldValue
-            weight.gram = doubleTextFieldValue * 1000
-            weight.ounce = doubleTextFieldValue * 35.274
-            weight.pound = doubleTextFieldValue * 2.20462
-            weight.s_pound = doubleTextFieldValue * 2.20462
-            weight.stone = doubleTextFieldValue / 6.350
-            
-            
-            ounceTextField.text = "\(weight.ounce)"
-            poundTextField.text = "\(weight.pound)"
-            gramTextField.text = "\(weight.gram)"
-            stoneTextField.text = "\(weight.stone)"
-            sPoundTextField.text = "\(weight.s_pound)"
-            
-        case .gram:
-            weight.kg = doubleTextFieldValue / 1000
-            weight.gram = doubleTextFieldValue
-            weight.ounce = doubleTextFieldValue / 28.35
-            weight.pound = doubleTextFieldValue / 453.592
-            weight.s_pound = doubleTextFieldValue / 453.592
-            weight.stone = doubleTextFieldValue / 6350.293
-            
-            
-            ounceTextField.text = "\(weight.ounce)"
-            poundTextField.text = "\(weight.pound)"
-            kgTextField.text = "\(weight.kg)"
-            stoneTextField.text = "\(weight.stone)"
-            sPoundTextField.text = "\(weight.s_pound)"
-            
         case .ounce:
+            weight.ounce = doubleTextFieldValue
             weight.kg = doubleTextFieldValue / 35.274
             weight.gram = doubleTextFieldValue * 28.35
-            weight.ounce = doubleTextFieldValue
             weight.pound = doubleTextFieldValue / 16
             weight.s_pound = doubleTextFieldValue / 16
             weight.stone = doubleTextFieldValue / 224
-            
             
             kgTextField.text = "\(weight.kg)"
             poundTextField.text = "\(weight.pound)"
@@ -106,54 +76,76 @@ class WeightViewController: UIViewController , UITextFieldDelegate{
             sPoundTextField.text = "\(weight.s_pound)"
             
         case .pound:
+            weight.ounce = doubleTextFieldValue * 16
             weight.kg = doubleTextFieldValue / 2.205
             weight.gram = doubleTextFieldValue * 453.592
-            weight.ounce = doubleTextFieldValue * 16
             weight.pound = doubleTextFieldValue
             weight.s_pound = doubleTextFieldValue
             weight.stone = doubleTextFieldValue / 14
             
-            
-            ounceTextField.text = "\(weight.ounce)"
             kgTextField.text = "\(weight.kg)"
+            ounceTextField.text = "\(weight.ounce)"
             gramTextField.text = "\(weight.gram)"
+            stoneTextField.text = "\(weight.stone)"
+            sPoundTextField.text = "\(weight.s_pound)"
+        
+        case .gram:
+            weight.ounce = doubleTextFieldValue / 28.35
+            weight.kg = doubleTextFieldValue / 1000
+            weight.gram = doubleTextFieldValue
+            weight.pound = doubleTextFieldValue / 453.592
+            weight.s_pound = doubleTextFieldValue / 453.592
+            weight.stone = doubleTextFieldValue / 6350.293
+            
+            kgTextField.text = "\(weight.kg)"
+            poundTextField.text = "\(weight.pound)"
+            ounceTextField.text = "\(weight.ounce)"
             stoneTextField.text = "\(weight.stone)"
             sPoundTextField.text = "\(weight.s_pound)"
             
         case .stone:
+            weight.ounce = doubleTextFieldValue * 224
             weight.kg = doubleTextFieldValue * 6.35
             weight.gram = doubleTextFieldValue * 6350.293
-            weight.ounce = doubleTextFieldValue * 224
-            weight.pound = doubleTextFieldValue
-            weight.s_pound = doubleTextFieldValue
+            weight.pound = doubleTextFieldValue * 14
             weight.stone = doubleTextFieldValue
+            weight.s_pound = doubleTextFieldValue * 14
             
-            
-            ounceTextField.text = "\(weight.ounce)"
+            kgTextField.text = "\(weight.kg)"
             poundTextField.text = "\(weight.pound)"
             gramTextField.text = "\(weight.gram)"
-            kgTextField.text = "\(weight.kg)"
+            ounceTextField.text = "\(weight.ounce)"
             sPoundTextField.text = "\(weight.s_pound)"
         
         case .s_pound:
-            weight.kg = doubleTextFieldValue
-            weight.gram = doubleTextFieldValue
-            weight.ounce = doubleTextFieldValue
+            weight.ounce = doubleTextFieldValue * 16
+            weight.kg = doubleTextFieldValue / 2.205
+            weight.gram = doubleTextFieldValue * 453.592
             weight.pound = doubleTextFieldValue
             weight.s_pound = doubleTextFieldValue
-            weight.stone = doubleTextFieldValue
+            weight.stone = doubleTextFieldValue / 14
             
+            kgTextField.text = "\(weight.kg)"
+            poundTextField.text = "\(weight.pound)"
+            gramTextField.text = "\(weight.gram)"
+            stoneTextField.text = "\(weight.stone)"
+            ounceTextField.text = "\(weight.ounce)"
+            
+        case .kg:
+            weight.ounce = doubleTextFieldValue * 35.274
+            weight.kg = doubleTextFieldValue
+            weight.gram = doubleTextFieldValue * 1000
+            weight.pound = doubleTextFieldValue * 2.20462
+            weight.stone = doubleTextFieldValue / 6.350
+            weight.s_pound = doubleTextFieldValue * 2.20462
             
             ounceTextField.text = "\(weight.ounce)"
             poundTextField.text = "\(weight.pound)"
             gramTextField.text = "\(weight.gram)"
             stoneTextField.text = "\(weight.stone)"
-            kgTextField.text = "\(weight.kg)"
+            sPoundTextField.text = "\(weight.s_pound)"
         }
-            
-
     }
-    
     
     
 }
